60a9a6f05a916d3b40701ab4fa931bed7edcaded
Chris <chris@cxi.org.uk>
2026-05-29 09:13:25 +0000

    Added missing import

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"github.com/caddyserver/caddy/v2"
 	"github.com/caddyserver/caddy/v2/caddyconfig/caddyfile"
 	"github.com/caddyserver/caddy/v2/modules/caddyhttp"
+	"github.com/caddyserver/caddy/v2/caddyconfig/httpcaddyfile"
 	"github.com/redis/go-redis/v9"
 )
 
